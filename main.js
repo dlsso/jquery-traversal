ac1a3a1e9484f94008ba6041e61a6a15dc6c512a
dlsso <supertonesfan@hotmail.com>
2014-06-27 09:58:28 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).on('ready', function() {
+	$('li').on('click', function() {
+  		console.log($(this))
+
+		// Set the color of the click 'li' to red
+		$(this).css('color', 'red')
+
+		// Find closest matching '.container'
+		// and set its background color to blue
+		$(this).closest('.container').css({
+			'background': 'blue',
+			'padding': 20
+		})
+
+		// Traverse up to find the closest '.container'
+		// then traverse down to find any '.featured' items
+		// and set the background of any results to green
+
+
+
+	})
+});
